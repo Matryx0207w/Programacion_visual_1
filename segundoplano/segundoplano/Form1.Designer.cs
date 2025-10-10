0e9a235bdeb5cf31293078555b616c8a1d26ecd5
Matryx0207w <67695546+Matryx0207w@users.noreply.github.com>
2025-10-10 08:24:02 -0600

    Add files via upload
    Trabajo de segundo plano

---
+++ segundoplano/segundoplano/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace OrdenamientoMultihilo
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.progressBurbuja = new System.Windows.Forms.ProgressBar();
+            this.progressQuickSort = new System.Windows.Forms.ProgressBar();
+            this.lblBurbuja = new System.Windows.Forms.Label();
+            this.lblQuickSort = new System.Windows.Forms.Label();
+            this.backgroundWorkerQuickSort = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numElementos = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTiempoBurbuja = new System.Windows.Forms.Label();
+            this.lblTiempoQuickSort = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numElementos)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(20, 60);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(120, 35);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar Datos";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(160, 60);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(120, 35);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar Ordenamiento";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // progressBurbuja
+            // 
+            this.progressBurbuja.Location = new System.Drawing.Point(20, 30);
+            this.progressBurbuja.Name = "progressBurbuja";
+            this.progressBurbuja.Size = new System.Drawing.Size(300, 23);
+            this.progressBurbuja.TabIndex = 2;
+            // 
+            // progressQuickSort
+            // 
+            this.progressQuickSort.Location = new System.Drawing.Point(20, 85);
+            this.progressQuickSort.Name = "progressQuickSort";
+            this.progressQuickSort.Size = new System.Drawing.Size(300, 23);
+            this.progressQuickSort.TabIndex = 3;
+            // 
+            // lblBurbuja
+            // 
+            this.lblBurbuja.AutoSize = true;
+            this.lblBurbuja.Location = new System.Drawing.Point(330, 35);
+            this.lblBurbuja.Name = "lblBurbuja";
+            this.lblBurbuja.Size = new System.Drawing.Size(65, 13);
+            this.lblBurbuja.TabIndex = 4;
+            this.lblBurbuja.Text = "Burbuja: 0%";
+            // 
+            // lblQuickSort
+            // 
+            this.lblQuickSort.AutoSize = true;
+            this.lblQuickSort.Location = new System.Drawing.Point(330, 90);
+            this.lblQuickSort.Name = "lblQuickSort";
+            this.lblQuickSort.Size = new System.Drawing.Size(75, 13);
+            this.lblQuickSort.TabIndex = 5;
+            this.lblQuickSort.Text = "QuickSort: 0%";
+            // 
+            // backgroundWorkerQuickSort
+            // 
+            this.backgroundWorkerQuickSort.WorkerReportsProgress = true;
+            this.backgroundWorkerQuickSort.WorkerSupportsCancellation = false;
+            this.backgroundWorkerQuickSort.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerQuickSort_DoWork);
+            this.backgroundWorkerQuickSort.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerQuickSort_ProgressChanged);
+            this.backgroundWorkerQuickSort.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerQuickSort_RunWorkerCompleted);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numElementos);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnGenerar);
+            this.groupBox1.Controls.Add(this.btnIniciar);
+            this.groupBox1.Location = new System.Drawing.Point(20, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 110);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controles";
+            // 
+            // numElementos
+            // 
+            this.numElementos.Location = new System.Drawing.Point(100, 25);
+            this.numElementos.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numElementos.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numElementos.Name = "numElementos";
+            this.numElementos.Size = new System.Drawing.Size(120, 20);
+            this.numElementos.TabIndex = 3;
+            this.numElementos.Value = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N° Elementos:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTiempoQuickSort);
+            this.groupBox2.Controls.Add(this.lblTiempoBurbuja);
+            this.groupBox2.Controls.Add(this.progressBurbuja);
+            this.groupBox2.Controls.Add(this.progressQuickSort);
+            this.groupBox2.Controls.Add(this.lblBurbuja);
+            this.groupBox2.Controls.Add(this.lblQuickSort);
+            this.groupBox2.Location = new System.Drawing.Point(20, 150);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(500, 150);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Progreso de Ordenamiento";
+            // 
+            // lblTiempoBurbuja
+            // 
+            this.lblTiempoBurbuja.AutoSize = true;
+            this.lblTiempoBurbuja.Location = new System.Drawing.Point(20, 60);
+            this.lblTiempoBurbuja.Name = "lblTiempoBurbuja";
+            this.lblTiempoBurbuja.Size = new System.Drawing.Size(113, 13);
+            this.lblTiempoBurbuja.TabIndex = 6;
+            this.lblTiempoBurbuja.Text = "Tiempo: No iniciado";
+            // 
+            // lblTiempoQuickSort
+            // 
+            this.lblTiempoQuickSort.AutoSize = true;
+            this.lblTiempoQuickSort.Location = new System.Drawing.Point(20, 115);
+            this.lblTiempoQuickSort.Name = "lblTiempoQuickSort";
+            this.lblTiempoQuickSort.Size = new System.Drawing.Size(113, 13);
+            this.lblTiempoQuickSort.TabIndex = 7;
+            this.lblTiempoQuickSort.Text = "Tiempo: No iniciado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 321);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Ordenamiento Multihilo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numElementos)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ProgressBar progressBurbuja;
+        private System.Windows.Forms.ProgressBar progressQuickSort;
+        private System.Windows.Forms.Label lblBurbuja;
+        private System.Windows.Forms.Label lblQuickSort;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerQuickSort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numElementos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblTiempoQuickSort;
+        private System.Windows.Forms.Label lblTiempoBurbuja;
+    }
+}
---
+++ segundoplano/segundoplano/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace OrdenamientoMultihilo
+{
+    public partial class Form1 : Form
+    {
+        private List<int> listaOriginal;
+        private List<int> listaBurbuja;
+        private List<int> listaQuick;
+        private Thread hiloBurbuja;
+        private Stopwatch relojBurbuja = new Stopwatch();
+        private Stopwatch relojQuick = new Stopwatch();
+        private bool ordenamientoEnProgreso = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ActualizarControles();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            int cantidad = (int)numElementos.Value;
+            Random rnd = new Random();
+            listaOriginal = new List<int>();
+
+            for (int i = 0; i < cantidad; i++)
+                listaOriginal.Add(rnd.Next(0, cantidad * 10));
+
+            MessageBox.Show($"Lista generada correctamente con {cantidad:N0} números.",
+                          "Datos Generados",
+                          MessageBoxButtons.OK,
+                          MessageBoxIcon.Information);
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (listaOriginal == null || listaOriginal.Count == 0)
+            {
+                MessageBox.Show("Primero genera los datos.",
+                              "Advertencia",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (ordenamientoEnProgreso)
+            {
+                MessageBox.Show("Ya hay un ordenamiento en progreso.",
+                              "Advertencia",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Reiniciar controles
+            progressBurbuja.Value = 0;
+            progressQuickSort.Value = 0;
+            lblBurbuja.Text = "Burbuja: 0%";
+            lblQuickSort.Text = "QuickSort: 0%";
+            lblTiempoBurbuja.Text = "Tiempo: Iniciando...";
+            lblTiempoQuickSort.Text = "Tiempo: Iniciando...";
+
+            // Copiamos la lista para cada algoritmo
+            listaBurbuja = new List<int>(listaOriginal);
+            listaQuick = new List<int>(listaOriginal);
+
+            ordenamientoEnProgreso = true;
+            ActualizarControles();
+
+            // Iniciar el hilo Burbuja
+            hiloBurbuja = new Thread(new ThreadStart(OrdenarBurbuja));
+            hiloBurbuja.IsBackground = true;
+            hiloBurbuja.Start();
+
+            // Iniciar el BackgroundWorker (QuickSort)
+            backgroundWorkerQuickSort.RunWorkerAsync(listaQuick);
+        }
+
+        private void OrdenarBurbuja()
+        {
+            try
+            {
+                relojBurbuja.Restart();
+                int n = listaBurbuja.Count;
+                int totalIteraciones = n - 1;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (listaBurbuja[j] > listaBurbuja[j + 1])
+                        {
+                            int temp = listaBurbuja[j];
+                            listaBurbuja[j] = listaBurbuja[j + 1];
+                            listaBurbuja[j + 1] = temp;
+                        }
+                    }
+
+                    // Reportar progreso (cada 100 iteraciones o en porcentajes específicos)
+                    if (i % 100 == 0 || i == totalIteraciones - 1)
+                    {
+                        int progreso = (int)((i / (float)totalIteraciones) * 100);
+                        ActualizarProgresoBurbuja(progreso);
+                    }
+                }
+
+                relojBurbuja.Stop();
+                ActualizarCompletadoBurbuja();
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    MessageBox.Show($"Error en Burbuja: {ex.Message}",
+                                  "Error",
+                                  MessageBoxButtons.OK,
+                                  MessageBoxIcon.Error);
+                }));
+            }
+        }
+
+        private void ActualizarProgresoBurbuja(int progreso)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<int>(ActualizarProgresoBurbuja), progreso);
+                return;
+            }
+
+            progressBurbuja.Value = Math.Min(progreso, 100);
+            lblBurbuja.Text = $"Burbuja: {progreso}%";
+            lblTiempoBurbuja.Text = $"Tiempo: {relojBurbuja.Elapsed:mm\\:ss\\.fff}";
+        }
+
+        private void ActualizarCompletadoBurbuja()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(ActualizarCompletadoBurbuja));
+                return;
+            }
+
+            progressBurbuja.Value = 100;
+            lblBurbuja.Text = "Burbuja: Completado";
+            lblTiempoBurbuja.Text = $"Tiempo: {relojBurbuja.ElapsedMilliseconds} ms";
+            VerificarCompletadoTotal();
+        }
+
+        // Métodos auxiliares para QuickSort
+        private void QuickSort(List<int> lista, int izquierda, int derecha, BackgroundWorker worker)
+        {
+            if (izquierda < derecha)
+            {
+                int pivot = Particionar(lista, izquierda, derecha);
+
+                // Reportar progreso basado en el tamaño del segmento
+                int progreso = (int)((derecha / (float)lista.Count) * 100);
+                worker.ReportProgress(Math.Min(progreso, 100));
+
+                QuickSort(lista, izquierda, pivot - 1, worker);
+                QuickSort(lista, pivot + 1, derecha, worker);
+            }
+        }
+
+        private int Particionar(List<int> lista, int izquierda, int derecha)
+        {
+            int pivote = lista[derecha];
+            int i = izquierda - 1;
+
+            for (int j = izquierda; j < derecha; j++)
+            {
+                if (lista[j] <= pivote)
+                {
+                    i++;
+                    int temp = lista[i];
+                    lista[i] = lista[j];
+                    lista[j] = temp;
+                }
+            }
+
+            int temp2 = lista[i + 1];
+            lista[i + 1] = lista[derecha];
+            lista[derecha] = temp2;
+            return i + 1;
+        }
+
+        // Eventos del BackgroundWorker
+        private void backgroundWorkerQuickSort_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                relojQuick.Restart();
+                List<int> lista = (List<int>)e.Argument;
+                QuickSort(lista, 0, lista.Count - 1, backgroundWorkerQuickSort);
+                relojQuick.Stop();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en QuickSort: {ex.Message}",
+                              "Error",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Error);
+            }
+        }
+
+        private void backgroundWorkerQuickSort_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressQuickSort.Value = e.ProgressPercentage;
+            lblQuickSort.Text = $"QuickSort: {e.ProgressPercentage}%";
+            lblTiempoQuickSort.Text = $"Tiempo: {relojQuick.Elapsed:mm\\:ss\\.fff}";
+        }
+
+        private void backgroundWorkerQuickSort_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressQuickSort.Value = 100;
+            lblQuickSort.Text = "QuickSort: Completado";
+            lblTiempoQuickSort.Text = $"Tiempo: {relojQuick.ElapsedMilliseconds} ms";
+            VerificarCompletadoTotal();
+        }
+
+        private void VerificarCompletadoTotal()
+        {
+            if (progressBurbuja.Value == 100 && progressQuickSort.Value == 100)
+            {
+                ordenamientoEnProgreso = false;
+                ActualizarControles();
+
+                MessageBox.Show($"Ordenamiento completado!\n\n" +
+                              $"Burbuja: {relojBurbuja.ElapsedMilliseconds} ms\n" +
+                              $"QuickSort: {relojQuick.ElapsedMilliseconds} ms",
+                              "Completado",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Information);
+            }
+        }
+
+        private void ActualizarControles()
+        {
+            btnIniciar.Enabled = !ordenamientoEnProgreso;
+            btnGenerar.Enabled = !ordenamientoEnProgreso;
+            numElementos.Enabled = !ordenamientoEnProgreso;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (ordenamientoEnProgreso)
+            {
+                DialogResult result = MessageBox.Show("Hay un ordenamiento en progreso. ¿Estás seguro de que quieres salir?",
+                                                    "Confirmar Salida",
+                                                    MessageBoxButtons.YesNo,
+                                                    MessageBoxIcon.Warning);
+
+                if (result == DialogResult.No)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                // Intentar detener los hilos de forma segura
+                try
+                {
+                    if (hiloBurbuja != null && hiloBurbuja.IsAlive)
+                        hiloBurbuja.Abort();
+
+                    if (backgroundWorkerQuickSort.IsBusy)
+                        backgroundWorkerQuickSort.CancelAsync();
+                }
+                catch
+                {
+                    // Ignorar excepciones al cerrar
+                }
+            }
+
+            base.OnFormClosing(e);
+        }
+    }
+}
